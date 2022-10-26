59b22a89a42fe50f551212b9c6d00fb5d65637e1
Carlos Villatoro <crvb0797@gmail.com>
2022-10-26 11:06:04 -0600

    Terminamos de implementar el front de la aplicación

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,91 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<!DOCTYPE html>
+<html lang="en">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Find easily a doctor and book online an appointment">
+	<meta name="author" content="Ansonika">
+	<title>Igresa a tu cuenta</title>
+
+    <!-- GOOGLE WEB FONT -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    
+	<!-- BASE CSS -->
+	<link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+	<link href="{{asset('css/style.css')}}" rel="stylesheet">
+	<link href="{{asset('css/menu.css')}}" rel="stylesheet">
+	<link href="{{asset('css/vendors.css')}}" rel="stylesheet">
+	<link href="{{asset('css/icon_fonts/css/all_icons_min.css')}}" rel="stylesheet">
+    
+	<!-- YOUR CUSTOM CSS -->
+	<link href="css/custom.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+	<div class="layer"></div>
+	<!-- Mobile menu overlay mask -->
+
+	<div id="preloader">
+		<div data-loader="circle-side"></div>
+	</div>
+	<!-- End Preload -->
+	
+	<main>
+		<div class="bg_color_2">
+			<div class="container margin_60_35">
+				<div id="login-2">
+					<h1>Igresa a Clinicas San Juan Apostol!</h1>
+					<form method="POST" action="{{ route('login') }}">
                         @csrf
+						<div class="box_form clearfix">
+							<div class="box_login last">
+								<div class="form-group">
+									<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Correo eléctronico">
+								</div>
+								<div class="form-group">
+									<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Contraseña">
+								</div>
+								<div class="form-group">
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+									<input class="btn_1" type="submit" value="Iniciar Sesión">
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+				<!-- /login -->
+			</div>
+		</div>
+	</main>
+	<!-- /main -->
+	
+	<footer>
+		<div class="container margin_60_35">
+			<hr>
+			<div class="row">
+				<div class="col-md-4">
+					<div id="copy">© 2022 Clinica Dental San Juan Apostol</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!--/footer-->
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+	<div id="toTop"></div>
+	<!-- Back to top button -->
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+	<!-- COMMON SCRIPTS -->
+	<script src="{{asset('js/jquery-3.5.1.min.js')}}"></script>
+	<script src="{{asset('js/common_scripts.min.js')}}"></script>
+	<script src="{{asset('js/functions.js')}}"></script>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+</body>
+</html>
 @endsection
--- resources/views/modulos/seleccionar.blade.php
+++ resources/views/modulos/seleccionar.blade.php
@@ -2,77 +2,183 @@
 
 @section('contenido')
 
-<section class="content">
-    <center>
-        <h1>Seleccione como desea ingresar al sistema</h1>
-    </center>
+<!DOCTYPE html>
+<html lang="es">
 
-    <div class="row">
-        {{-- SECRETARIA --}}
-        <div class="col-lg-3 col-xs-6">
-            <div class="small-box" style="background-color: #f781d8; color:white">
-                <div class="inner">
-                    <h3>Secretaria</h3>
-                    <p>Inicie sesión</p>
-                </div>
-                <div class="icon">
-                    <i class="fa fa-female"></i>
-                </div>
-                <a href="{{route('login')}}" class="small-box-footer">
-                    Ingresar <div class="fa fa-arrow-circle-right"></div>
-                </a>
-            </div>
-        </div>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Encuentra a tu desntista ideal desde la comodidad de tu casa">
+	<meta name="author" content="Universidad Mariano Galvez de Guatemala">
+	<title>Clinica Dental San Juan Apostol</title>
 
-        {{-- DOCTOR --}}
-        <div class="col-lg-3 col-xs-6">
-            <div class="small-box" style="background-color: #bdbdbd; color:white">
-                <div class="inner">
-                    <h3>Doctor</h3>
-                    <p>Inicie sesión</p>
-                </div>
-                <div class="icon">
-                    <i class="fa fa-user-md"></i>
-                </div>
-                <a href="{{route('login')}}" class="small-box-footer">
-                    Ingresar <div class="fa fa-arrow-circle-right"></div>
-                </a>
-            </div>
-        </div>
+	   
+    <!-- GOOGLE WEB FONT -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    
+	<!-- BASE CSS -->
+	<link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+	<link href="{{asset('css/style.css')}}" rel="stylesheet">
+	<link href="{{asset('css/menu.css')}}" rel="stylesheet">
+	<link href="{{asset('css/vendors.css')}}" rel="stylesheet">
+	<link href="{{asset('css/icon_fonts/css/all_icons_min.css')}}" rel="stylesheet">
+    
+</head>
 
-        {{-- PACIENTE --}}
-        <div class="col-lg-3 col-xs-6">
-            <div class="small-box bg-yellow">
-                <div class="inner">
-                    <h3>Paciente</h3>
-                    <p>Inicie sesión</p>
-                </div>
-                <div class="icon">
-                    <i class="fa fa-users"></i>
-                </div>
-                <a href="{{route('login')}}" class="small-box-footer">
-                    Ingresar <div class="fa fa-arrow-circle-right"></div>
-                </a>
-            </div>
-        </div>
+<body>
 
-         {{-- ADMINISTRADOR --}}
-         <div class="col-lg-3 col-xs-6">
-            <div class="small-box bg-blue">
-                <div class="inner">
-                    <h3>Administrador</h3>
-                    <p>Inicie sesión</p>
-                </div>
-                <div class="icon">
-                    <i class="fa fa-male"></i>
-                </div>
-                <a href="{{route('login')}}" class="small-box-footer">
-                    Ingresar <div class="fa fa-arrow-circle-right"></div>
-                </a>
-            </div>
-        </div>
+	<div class="layer"></div>
+	<!-- Mobile menu overlay mask -->
 
-    </div>
-</section>
+	<div id="preloader">
+		<div data-loader="circle-side"></div>
+	</div>
+	<!-- End Preload -->
+    
+	<header class="header_sticky">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-6">
+					<div id="logo_home">
+						<h1><a href="/" title="Findoctor">Clinica Dental San Juan Apostol</a></h1>
+					</div>
+				</div>
+				<nav class="col-lg-9 col-6">
+					<a class="cmn-toggle-switch cmn-toggle-switch__htx open_close" href="#0"><span>Menu mobile</span></a>
+					<ul id="top_access">
+						<li><a href="{{route('login')}}"><i class="pe-7s-user"></i></a></li>
+					</ul>
+					<!-- /main-menu -->
+				</nav>
+			</div>
+		</div>
+		<!-- /container -->
+	</header>
+	<!-- /header -->
+	
+	<main>
+		<div class="hero_home version_1">
+			<div class="content">
+				<h3>Encuentra a tu dentista ideal!</h3>
+				<p>
+					Has tu cita de forma totalmente gratuita con el dentista de tu preferencia
+				</p>
+			</div>
+		</div>
+		<!-- /Hero -->
+
+		<div class="container margin_120_95">
+			<div class="main_title">
+				<h2>Descubre las citas <strong>online</strong>!</h2>
+				<p>Ten tu propia cuenta dentro de la plataforma y lleva el control de tus citas dentro del historial de tu perfil</p>
+			</div>
+			<div class="row add_bottom_30">
+				<div class="col-lg-4">
+					<div class="box_feat" id="icon_1">
+						<span></span>
+						<h3>Encuentra a tu dentista</h3>
+						<p>Encuentra a un dentista de tu preferencia</p>
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="box_feat" id="icon_2">
+						<span></span>
+						<h3>Revisa sus horarios</h3>
+						<p>Revisa los horarios de los doctores y programa tu cita</p>
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="box_feat" id="icon_3">
+						<h3>Agenda tu cita</h3>
+						<p>Agenda tu cita con tan solo 1 clic</p>
+					</div>
+				</div>
+			</div>
+			<!-- /row -->
+		</div>
+		<!-- /container -->
+
+		<div class="bg_color_1">
+			<div class="container margin_120_95">
+				<div class="main_title">
+					<h2>Nuestros productos y servicios</h2>
+					<p>Estos son las promociones del mes, de la semana o del día se actualizan todos los días</p>
+				</div>
+				<div id="reccomended" class="owl-carousel owl-theme">
+					<div class="item">
+						<a href="detail-page.html">
+							<div class="title">
+							</div><img src="http://via.placeholder.com/350x500.jpg" alt="">
+						</a>
+					</div>
+
+                    <div class="item">
+						<a href="detail-page.html">
+							<div class="title">
+							</div><img src="http://via.placeholder.com/350x500.jpg" alt="">
+						</a>
+					</div>
+
+                    <div class="item">
+						<a href="detail-page.html">
+							<div class="title">
+							</div><img src="http://via.placeholder.com/350x500.jpg" alt="">
+						</a>
+					</div>
+
+                    <div class="item">
+						<a href="detail-page.html">
+							<div class="title">
+							</div><img src="http://via.placeholder.com/350x500.jpg" alt="">
+						</a>
+					</div>
+
+                    <div class="item">
+						<a href="detail-page.html">
+							<div class="title">
+							</div><img src="http://via.placeholder.com/350x500.jpg" alt="">
+						</a>
+					</div>
+
+                    <div class="item">
+						<a href="detail-page.html">
+							<div class="title">
+							</div><img src="http://via.placeholder.com/350x500.jpg" alt="">
+						</a>
+					</div>
+					
+				</div>
+				<!-- /carousel -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /white_bg -->
+	</main>
+	<!-- /main content -->
+	
+	<footer>
+		<div class="container margin_60_35">
+			<hr>
+			<div class="row">
+				<div class="col-md-4">
+					<div id="copy">© 2022 Clinica Dental San Juan Apostol</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!--/footer-->
+
+	<div id="toTop"></div>
+	<!-- Back to top button -->
+
+	<!-- COMMON SCRIPTS -->
+	<script src="{{asset('js/jquery-3.5.1.min.js')}}"></script>
+	<script src="{{asset('js/common_scripts.min.js')}}"></script>
+	<script src="{{asset('js/functions.js')}}"></script>
+
+</body>
+
+</html>
 
 @endsection
